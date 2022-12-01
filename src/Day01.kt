1d66ad3dea7d21dd1fbcf5daa8364397a187dcdb
Dave Dupplaw <david.dupplaw@siemens.com>
2022-12-01 15:54:14 +0000

    feat: DD: Day01 Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,28 @@
 fun main() {
+    fun getElvesWithCalories(input: List<String>): Map<Int, Int> {
+        var elfNumber = 0
+        return input.mapNotNull { i ->
+            if (i.isBlank()) {
+                elfNumber++; null
+            } else elfNumber to i.toInt()
+        }
+                .groupBy({ it.first }, { it.second })
+                .mapValues { it.value.sum() }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return getElvesWithCalories(input)
+                .values
+                .max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getElvesWithCalories(input)
+                .values
+                .sortedDescending()
+                .take(3)
+                .sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
