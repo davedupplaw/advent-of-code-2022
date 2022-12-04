7e7f93faa17a88ffc3a186e25e0934f0ab0f4e80
David Dupplaw <david@dupplaw.me.uk>
2022-12-04 12:10:12 +0000

    feat: DD/WD: Day03 Part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -19,17 +19,39 @@ fun main() {
             .sumOf { convertToPriorities(it) }
     }
 
+    fun findCommonItems(rucksack1: Set<Char>, rucksack2: String): Set<Char> {
+        return rucksack1.mapNotNull { item ->
+            if (rucksack2.contains(item)) item else null
+        }.toSet()
+    }
+
+    fun findCommonItemInGroup(group: List<String>): Char {
+        val firstRucksack = group[0]
+        var foundItems = firstRucksack.toCharArray().toSet()
+
+        for (rucksack in group.drop(1)) {
+            foundItems = findCommonItems(foundItems, rucksack)
+        }
+
+        return foundItems.toList()[0]
+    }
+
     fun part2(input: List<String>): Int {
-        return 1
+        return input
+            .chunked(3)
+            .map { findCommonItemInGroup(it) }
+            .sumOf { convertToPriorities(it) }
     }
 
     val test = readInput("Day03.test")
     val part1 = part1(test)
-    println(part1)
+    println("part1: $part1")
     check(part1 == 157)
-//    check(part2(test) == 12)
+    val part2 = part2(test)
+    println("part2: $part2")
+    check(part2 == 70)
 
     val input = readInput("Day03")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
