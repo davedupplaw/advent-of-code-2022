1d058576dd163a2bdb13bcaa7103a714dbc7db2e
Dave Dupplaw <david.dupplaw@siemens.com>
2022-12-05 09:12:51 +0000

    feat: DD: Day05 Part 1

---
+++ src/Day05.kt
@@ -0,0 +1,77 @@
+fun main() {
+    fun readCrateStack(input: List<String>, indexOfBlankLine: Int): MutableMap<Int, List<String>> {
+        val stack = input
+                .take(indexOfBlankLine)
+                .map {
+                    it.chunked(4)
+                            .mapIndexed { index, crate ->
+                                index to crate.replace("[", "").replace("]", "").trim()
+                            }
+                            .filter { p -> p.second.isNotBlank() }
+                }
+                .dropLast(1)
+                .flatten()
+                .groupBy { it.first + 1 }
+                .mapValues { it.value.reversed().map { v -> v.second } }
+                .toMutableMap()
+        return stack
+    }
+
+    fun readMoves(input: List<String>, indexOfBlankLine: Int): List<Triple<Int, Int, Int>> {
+        val regex = """move (\d+) from (\d+) to (\d+)""".toRegex()
+        return input
+                .drop(indexOfBlankLine)
+                .filter { it.isNotBlank() }
+                .map {
+                    val values = regex.find(it)!!.groupValues
+                    Triple(values[1].toInt(), values[2].toInt(), values[3].toInt())
+                }
+    }
+
+
+    fun processMoves(stack: MutableMap<Int, List<String>>, moves: List<Triple<Int, Int, Int>>) {
+        moves.forEach { move ->
+            repeat(move.first) {
+                val topOfStack = stack[move.second]!!.last()
+                stack[move.third] = stack[move.third]!!.plus(topOfStack)
+                stack[move.second] = stack[move.second]!!.dropLast(1)
+            }
+        }
+    }
+
+    fun getTopOfStacks(stack: MutableMap<Int, List<String>>): List<String> {
+        println("At end: $stack")
+        return stack.entries.sortedBy { it.key }.map { it.value.lastOrNull() ?: " " }
+    }
+
+    fun part1(input: List<String>): String {
+        val indexOfBlankLine = input.indexOfFirst { it.isBlank() }
+        val stack = readCrateStack(input, indexOfBlankLine)
+        val moves = readMoves(input, indexOfBlankLine)
+        processMoves(stack, moves)
+
+        return getTopOfStacks(stack).joinToString("") { it }
+    }
+
+    fun part2(input: List<String>): Int {
+        return 2
+    }
+
+    val test = readInput("Day05.test")
+
+    val part1test = part1(test)
+    println("part1 test: $part1test")
+    check(part1test == "CMZ")
+
+//    val part2test = part2(test)
+//    println("part2 test: $part2test")
+//    check(part2test == 70)
+
+    val input = readInput("Day05")
+
+    val part1 = part1(input)
+    println("Part1: $part1")
+
+//    val part2 = part2(input)
+//    println("Part2: $part2")
+}
